6ca320099c755f0113daa3e0ff0623ec9de6f002
wenjia.wu <wenjia.wu@thoughtworks.com>
2020-07-03 22:49:28 +0800

    finish all practice.

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,5 @@
+import getUrl from "./utils";
+import { ADDRESS, REGION, PATH, PORT } from "./constant";
 
+const url = getUrl(ADDRESS, PORT, PATH);
+export { REGION as region, url };
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,16 @@
-export const getPoetry
+export const getPoetry = () => {
+  return fetch("https://v1.jinrishici.com/all.json")
+    .then((response) => response.json())
+    .then((data) => [data.origin, data.author, data.content])
+    .catch((error) => console.log(error));
+};
+
+// export async function getPoetry() {
+//     try {
+//         const response = await fetch('https://v1.jinrishici.com/all.json');
+//         const data = await response.json();
+//         return [data.origin, data.author, data.content];
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
